cb60795ee2fcb8da7cfbda68111a4d0c4fdfb7be
CodingCat <zhunansjtu@gmail.com>
2015-05-21 14:40:48 -0400

    finer grained lock

--- src/main/scala/cpslab/deploy/benchmark/LockStrippingHashMap.scala
+++ src/main/scala/cpslab/deploy/benchmark/LockStrippingHashMap.scala
@@ -9,31 +9,31 @@ import scala.collection.mutable.ListBuffer
 import cpslab.lsh.vector.SparseVector
 
 class LockStrippingHashMap {
-  val locks = Array.fill[Lock](196)(new ReentrantLock)
+  val locks = Array.fill[Lock](8192)(new ReentrantLock)
 
   val store = new mutable.HashMap[Int, ListBuffer[SparseVector]]
 
   def put(key: Int, value: SparseVector): Unit = {
     try {
-      locks(key % 196).lock()
+      locks(key % 8192).lock()
       store.getOrElseUpdate(key, new ListBuffer[SparseVector]) += value
     } catch {
       case x: Exception =>
         x.printStackTrace()
     } finally {
-      locks(key % 196).unlock()
+      locks(key % 8192).unlock()
     }
   }
 
   def get(key: Int) = {
     try {
-      locks(key % 196).lock()
+      locks(key % 8192).lock()
       store.get(key)
     } catch {
       case x: Exception =>
         x.printStackTrace()
     } finally {
-      locks(key % 196).unlock()
+      locks(key % 8192).unlock()
     }
   }
 }
