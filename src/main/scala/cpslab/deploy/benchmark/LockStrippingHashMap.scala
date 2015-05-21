d8a31b66ddd7e8cd39a64bc64f24a46d72952c0a
CodingCat <zhunansjtu@gmail.com>
2015-05-21 15:12:00 -0400

    read write lock

--- src/main/scala/cpslab/deploy/benchmark/LockStrippingHashMap.scala
+++ src/main/scala/cpslab/deploy/benchmark/LockStrippingHashMap.scala
@@ -1,7 +1,7 @@
 package cpslab.deploy.benchmark
 
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.{ReentrantLock, Condition, Lock}
+import java.util.concurrent.locks._
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
@@ -9,31 +9,31 @@ import scala.collection.mutable.ListBuffer
 import cpslab.lsh.vector.SparseVector
 
 class LockStrippingHashMap {
-  val locks = Array.fill[Lock](8192)(new ReentrantLock)
+  val locks = Array.fill[ReentrantReadWriteLock](8192)(new ReentrantReadWriteLock(false))
 
   val store = new mutable.HashMap[Int, ListBuffer[SparseVector]]
 
   def put(key: Int, value: SparseVector): Unit = {
     try {
-      locks(key % 8192).lock()
+      locks(key % 8192).writeLock().lock()
       store.getOrElseUpdate(key, new ListBuffer[SparseVector]) += value
     } catch {
       case x: Exception =>
         x.printStackTrace()
     } finally {
-      locks(key % 8192).unlock()
+      locks(key % 8192).writeLock().unlock()
     }
   }
 
   def get(key: Int) = {
     try {
-      locks(key % 8192).lock()
+      locks(key % 8192).readLock().lock()
       store.get(key)
     } catch {
       case x: Exception =>
         x.printStackTrace()
     } finally {
-      locks(key % 8192).unlock()
+      locks(key % 8192).readLock().unlock()
     }
   }
 }
