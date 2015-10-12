d2630f212934d6f850cb3af132d00baf81690557
CodingCat <zhunansjtu@gmail.com>
2015-10-12 07:44:35 -0400

    use customized ID in async write test

--- src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
+++ src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
@@ -128,9 +128,9 @@ object HashTreeTest {
         if (cnt > cap * threadNumber) {
           return
         }
-        val (id, size, indices, values) = Vectors.fromString1(line)
-        val vector = new SparseVector(id, size, indices, values)
-        vectorIdToVector.put(vector.vectorId, vector)
+        val (_, size, indices, values) = Vectors.fromString1(line)
+        val vector = new SparseVector(cnt, size, indices, values)
+        vectorIdToVector.put(cnt, vector)
       }
     }
     ActorBasedPartitionedHTreeMap.actorSystem.actorOf(
