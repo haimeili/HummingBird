c4517f9afe1d19db570536db5d63a0295a6a6197
CodingCat <zhunansjtu@gmail.com>
2015-10-13 23:13:11 -0400

    HashBitExtractingPartitioner

--- src/main/scala/cpslab/utils/Partitioner.scala
+++ src/main/scala/cpslab/utils/Partitioner.scala
@@ -10,13 +10,12 @@ class HashPartitioner[K](numPartitions: Int) extends Partitioner[K](numPartition
   }
 }
 
-class RangePartitioner[K : Ordering : ClassTag](numPartitions: Int)
-  extends Partitioner[K](numPartitions) {
-
-  private var ordering = implicitly[Ordering[K]]
+class HashBitExtractingPartitioner[K](numBits: Int)
+  extends Partitioner[K](1 << (numBits + 1)) {
 
   override def getPartition(value: K): Int = {
-    //TODO: finish the range partitioner
-    0
+    val objHashValue = value.asInstanceOf[Int].hashCode()
+    val partitionId = objHashValue >>> (32 - numBits)
+    partitionId
   }
 }
