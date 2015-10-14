2d2161ba7e3c34ae00c11752ca898aedcbf2eb69
CodingCat <zhunansjtu@gmail.com>
2015-10-14 14:33:52 -0400

    test

--- src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
+++ src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
@@ -449,8 +449,7 @@ object HashTreeTest {
     println(sortedDistances.toList)
     //step 2: calculate the distance of the ground truth
     val groundTruth = new ListBuffer[Double]
-    println(existingID.size())
-    val itr = existingID.iterator()
+    val itr = existingID.toList.distinct.iterator
     while (itr.hasNext) {
       val vId = itr.next()
       val vector = vectorIdToVector.get(vId)
