0f40339001b1d7d35caea90f3e288886dc479786
CodingCat <zhunansjtu@gmail.com>
2016-06-05 11:24:12 -0400

    fix start time

--- src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
+++ src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
@@ -334,7 +334,6 @@ object HashTreeTest {
     ActorBasedPartitionedHTreeMap.actorSystem = ActorSystem("AK", conf)
     implicit val executionContext = ActorBasedPartitionedHTreeMap.actorSystem.dispatchers.lookup(
       "akka.actor.writer-dispatcher")
-    val st = System.nanoTime()
     val mainFs = taskQueue.map {
       vector =>
         Future {
@@ -348,6 +347,7 @@ object HashTreeTest {
             }
         }
     }
+    val st = System.nanoTime()
     Future.sequence(mainFs).onComplete {
       case Success(result)  =>
         // do nothing
