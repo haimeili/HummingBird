83b090c96e6d6146fe0447f5c0c32a465fddd7fb
CodingCat <zhunansjtu@gmail.com>
2016-06-20 07:13:49 -0400

    fix

--- src/main/scala/cpslab/deploy/ShardDatabase.scala
+++ src/main/scala/cpslab/deploy/ShardDatabase.scala
@@ -119,7 +119,7 @@ private[cpslab] object ShardDatabase extends DataSetLoader {
     val lockScale = conf.getInt("cpslab.lsh.btree.lockScale")
     val nodeSize = conf.getInt("cpslab.lsh.btree.nodeSize")
     val db = DBMaker.memoryUnsafeDB().transactionDisable().lockScale(lockScale)make()
-    vectorIdToVectorBTree = db.treeMapCreate("vectorIdToVector").valuesOutsideNodesEnable().
+    vectorIdToVectorBTree = db.treeMapCreate("vectorIdToVector").
       keySerializer(Serializers.IntSerializer).valueSerializer(Serializers.VectorSerializer).
       nodeSize(nodeSize).make[Int, SparseVector]()
     vectorDatabaseBTree = new Array[BTreeMap[Long, LSHBTreeVal]](tableNum)
