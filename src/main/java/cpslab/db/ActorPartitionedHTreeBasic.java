67d48f6ec69bcda98c6afd8f4be8f56e86832a6e
CodingCat <zhunansjtu@gmail.com>
2016-06-27 22:13:21 -0400

    test

--- src/main/java/cpslab/db/ActorPartitionedHTreeBasic.java
+++ src/main/java/cpslab/db/ActorPartitionedHTreeBasic.java
@@ -285,7 +285,8 @@ public class ActorPartitionedHTreeBasic<K, V> extends PartitionedHTreeMap<K, V> 
             throw new DBException.DataCorruption("cyclic reference in linked list");
           bucketConflictCost++;
           if (!(hasher instanceof LocalitySensitiveHasher)) {
-            System.out.println("hash code:" + h + ", bucketConflictCost:" + bucketConflictCost);
+            System.out.println("key: " + key + ", hash code:" + h + ", bucketConflictCost:" +
+                    bucketConflictCost);
           }
           if (CC.ASSERT && bucketConflictCost > 1024 * 1024)
             throw new DBException.DataCorruption("linked list too large");
