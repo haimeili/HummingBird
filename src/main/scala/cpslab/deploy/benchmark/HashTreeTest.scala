2e819120891bcea11ccdef3615bf192e9671c4a9
CodingCat <zhunansjtu@gmail.com>
2016-09-25 13:22:37 -0400

    test

--- src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
+++ src/main/scala/cpslab/deploy/benchmark/HashTreeTest.scala
@@ -124,6 +124,14 @@ object HashTreeTest {
         }
         if (!foundNull) {
           println("ALL VECTOR WRITTEN SUCCESSFULLY")
+          for (i <- 0 until totalWriteCount) {
+            trainingIDs += i
+          }
+          for (i <- 0 until totalWriteCount) {
+            testIDs += i
+          }
+          testAccuracy(conf)
+          sys.exit(0)
         }
       }
       if (ifRunReadTest && mainMsgNum >= totalWriteCount &&
@@ -269,11 +277,11 @@ object HashTreeTest {
     var cnt = 0
     val taskQueue = new ListBuffer[SparseVector]
     for (file <- allFiles; line <- Source.fromFile(file).getLines()) {
-      val (_, size, indices, values) = Vectors.fromString1(line)
+      val (id, size, indices, values) = Vectors.fromString(line)
       val squareSum = math.sqrt(values.foldLeft(0.0) {
         case (sum, weight) => sum + weight * weight
       })
-      val vector = new SparseVector(cnt, size, indices,
+      val vector = new SparseVector(id, size, indices,
         values.map(_ / squareSum))
       taskQueue += vector
       cnt += 1
