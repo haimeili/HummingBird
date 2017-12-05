32fd4a40bc7241c8c4f4d05609aff0378878e17d
haimeili <lihaimei880313@gmail.com>
2017-12-04 23:20:09 -0800

    shorten some lines

--- src/test/scala/cpslab/lsh/vector/SimilarityCalculatorSuite.scala
+++ src/test/scala/cpslab/lsh/vector/SimilarityCalculatorSuite.scala
@@ -5,18 +5,21 @@ import org.scalatest.FunSuite
 class SimilarityCalculatorSuite extends FunSuite {
   
   test("fast similarity calculation") {
-    val vector1 = Vectors.sparse(10, Seq((0, 1.0), (2, 1.0), (3, 1.0),(9, 1.0))).asInstanceOf[SparseVector]
+    val vector1 = Vectors.sparse(10, Seq((0, 1.0), (2, 1.0), (3, 1.0),(9, 1.0)))
+      .asInstanceOf[SparseVector]
     val vector2 = Vectors.sparse(10, Seq((2, 1.0), (3, 1.0))).asInstanceOf[SparseVector]
     val sim = SimilarityCalculator.fastCalculateSimilarity(vector1, vector2)
     assert(sim === 2.0)
-    val vector3 = Vectors.sparse(10, Seq((0, 1.0), (2, 1.0), (3, 1.0),(9, 1.0))).asInstanceOf[SparseVector]
+    val vector3 = Vectors.sparse(10, Seq((0, 1.0), (2, 1.0), (3, 1.0),(9, 1.0)))
+      .asInstanceOf[SparseVector]
     val vector4 = Vectors.sparse(10, Seq((5, 1.0), (6, 1.0))).asInstanceOf[SparseVector]
     val sim1 = SimilarityCalculator.fastCalculateSimilarity(vector3, vector4)
     assert(sim1 === 0.0)
   }
 
   test("fast similarity calculation when there is a mis-matched bit in the middle of the vector") {
-    val vector3 = Vectors.sparse(10, Seq((0, 1.0), (2, 1.0), (3, 1.0),(9, 1.0))).asInstanceOf[SparseVector]
+    val vector3 = Vectors.sparse(10, Seq((0, 1.0), (2, 1.0), (3, 1.0),(9, 1.0)))
+      .asInstanceOf[SparseVector]
     val vector4 = Vectors.sparse(10, Seq((3, 1.0), (6, 1.0), (9, 1.0))).asInstanceOf[SparseVector]
     val sim1 = SimilarityCalculator.fastCalculateSimilarity(vector3, vector4)
     assert(sim1 === 2.0)
